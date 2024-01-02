58652f7679c492cd962a274e0df035fe9dbd7fba
Yafet Zerihun <zerihunyafet80@gmail.com>
2024-01-02 17:24:35 +0300

    animation on tab hover

--- src/sections/Features.tsx
+++ src/sections/Features.tsx
@@ -1,7 +1,11 @@
 "use client";
-import { DotLottiePlayer } from "@dotlottie/react-player";
+import {
+  DotLottieCommonPlayer,
+  DotLottiePlayer,
+} from "@dotlottie/react-player";
 import productImage from "@/assets/product-image.png";
 import Image from "next/image";
+import { useRef } from "react";
 
 const tabs = [
   {
@@ -30,6 +34,38 @@ const tabs = [
   },
 ];
 
+const FeatureTab = (tab: (typeof tabs)[number]) => {
+  const dotLottieRef = useRef<DotLottieCommonPlayer>(null);
+
+  const handleTabHover = () => {
+    if (dotLottieRef.current === null) return;
+    dotLottieRef.current.seek(0);
+    dotLottieRef.current.play();
+  };
+  return (
+    <div
+      onMouseEnter={handleTabHover}
+      key={tab.title}
+      className="border border-white/15 flex flex-col lg:flex-row justify-center items-center flex-1 p-2.5 rounded-xl gap-2.5"
+    >
+      <div className="h-12 w-12 border border-white/15 rounded-lg inline-flex items-center justify-center">
+        <DotLottiePlayer
+          ref={dotLottieRef}
+          src={tab.icon}
+          className="h-5 w-5"
+          autoplay
+        />
+      </div>
+      <div className="font-medium">{tab.title}</div>
+      {tab.isNew && (
+        <div className="text-xs px-2 max-h-4 bg-purple-700 text-black font-semibold rounded-full">
+          new
+        </div>
+      )}
+    </div>
+  );
+};
+
 export const Features = () => {
   return (
     <section className="py-20 md:py-24">
@@ -43,20 +79,7 @@ export const Features = () => {
         </p>
         <div className="mt-10 flex flex-col lg:flex-row gap-3">
           {tabs.map((tab) => (
-            <div
-              key={tab.title}
-              className="border border-white/15 flex flex-col lg:flex-row justify-center items-center flex-1 p-2.5 rounded-xl gap-2.5"
-            >
-              <div className="h-12 w-12 border border-white/15 rounded-lg inline-flex items-center justify-center">
-                <DotLottiePlayer src={tab.icon} className="h-5 w-5" autoplay />
-              </div>
-              <div className="font-medium">{tab.title}</div>
-              {tab.isNew && (
-                <div className="text-xs px-2 max-h-4 bg-purple-700 text-black font-semibold rounded-full">
-                  new
-                </div>
-              )}
-            </div>
+            <FeatureTab {...tab} key={tab.title} />
           ))}
 
           {/* <Image src={productImage} alt="product iamge" /> */}
--- src/sections/LogoTicker.tsx
+++ src/sections/LogoTicker.tsx
@@ -1,9 +1,11 @@
+"use client";
 import acmeLogo from "@/assets/logo-acme.png";
 import apexLogo from "@/assets/logo-apex.png";
 import celestialLogo from "@/assets/logo-celestial.png";
 import quantumLogo from "@/assets/logo-quantum.png";
 import pulseLogo from "@/assets/logo-pulse.png";
 import echoLogo from "@/assets/logo-echo.png";
+import { motion } from "framer-motion";
 
 export const LogoTicker = () => {
   return (
@@ -13,9 +15,28 @@ export const LogoTicker = () => {
           <div className="flex-1 md:flex-none">
             <h2>Trusted by top innovate teams</h2>
           </div>
-          <div className="flex-1 overflow-hidden [mask-image:linear-gradient(to_right,transparent,black_20%,black_80%,transparent)]">
-            <div className="flex flex-none gap-14">
+          <div className="flex flex-1 overflow-hidden [mask-image:linear-gradient(to_right,transparent,black_20%,black_80%,transparent)]">
+            <motion.div
+              initial={{
+                translateX: "-50%",
+              }}
+              animate={{
+                translateX: "0",
+              }}
+              transition={{
+                repeat: Infinity,
+                duration: 30,
+                ease: "linear",
+              }}
+              className="flex flex-none gap-14 pr-14 -translate-x-1/2"
+            >
               {[
+                acmeLogo,
+                pulseLogo,
+                echoLogo,
+                quantumLogo,
+                celestialLogo,
+                apexLogo,
                 acmeLogo,
                 pulseLogo,
                 echoLogo,
@@ -25,7 +46,7 @@ export const LogoTicker = () => {
               ].map((logo, index) => (
                 <img src={logo.src} key={index} className="h-6 w-auto" />
               ))}
-            </div>
+            </motion.div>
           </div>
         </div>
       </div>
