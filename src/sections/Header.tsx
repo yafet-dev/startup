7ca661a847c40bee7939a750575cc3686e345fa2
Yafet Zerihun <zerihunyafet80@gmail.com>
2024-10-04 13:33:14 +0300

    style cta button

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -13,10 +13,14 @@ export const Header = () => {
 
             <div className="flex gap-4 items-center">
               <button
-                className="border py-2 px-3 rounded-lg font-medium
-              text-sm bg-gradient-to-b from-[#190d2e] to-[#4a208a]"
+                className="relative py-2 px-3 rounded-lg font-medium
+              text-sm bg-gradient-to-b from-[#190d2e] to-[#4a208a] shadow-[0px_0px_12px_#8c45ff]"
               >
-                Join waitlist
+                <div className="absolute inset-0">
+                  <div className="border border-white/20 aboslute inset-0 [mask-image:linear-gradient(to_bottom,black,transparent)]"></div>
+                  <div className="border absolute inset-0 border-white/40 [mask-image:linear-gradient(to_top,black,transparent)]"></div>
+                </div>
+                <span>Join waitlist</span>
               </button>
               <span>
                 <MenuIcon />
