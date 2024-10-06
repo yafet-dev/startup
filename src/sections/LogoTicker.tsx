83630600d823b0974780b693bd24fb0c7e28067f
Yafet Zerihun <zerihunyafet80@gmail.com>
2024-10-07 00:09:08 +0300

    add all static files

--- src/sections/LogoTicker.tsx
+++ src/sections/LogoTicker.tsx
@@ -1,6 +1,9 @@
 import acmeLogo from "@/assets/logo-acme.png";
 import apexLogo from "@/assets/logo-apex.png";
 import celestialLogo from "@/assets/logo-celestial.png";
+import quantumLogo from "@/assets/logo-quantum.png";
+import pulseLogo from "@/assets/logo-pulse.png";
+import echoLogo from "@/assets/logo-echo.png";
 
 export const LogoTicker = () => {
   return (
