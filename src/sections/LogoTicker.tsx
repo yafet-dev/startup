800c418305cb123a73f9f8d844f6e305fdb6e4b1
Yafet Zerihun <zerihunyafet80@gmail.com>
2024-10-07 00:07:47 +0300

    add logos

--- src/sections/LogoTicker.tsx
+++ src/sections/LogoTicker.tsx
@@ -1,9 +1,13 @@
+import acmeLogo from "@/assets/logo-acme.png";
+import apexLogo from "@/assets/logo-apex.png";
+import celestialLogo from "@/assets/logo-celestial.png";
+
 export const LogoTicker = () => {
   return (
     <section>
       <div className="container">
         <div>
-          <div>Trusted by top innovate teams</div>
+          <h2>Trusted by top innovate teams</h2>
         </div>
       </div>
     </section>
