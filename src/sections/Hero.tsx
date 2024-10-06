e01f8be09e5377272535369d0387c0a786a4d960
Yafet Zerihun <zerihunyafet80@gmail.com>
2024-10-06 20:20:54 +0300

    style the sphere

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -9,8 +9,11 @@ export const Hero = () => {
         backgroundImage: `url(${starsBg.src})`,
       }}
     >
-      <div className="absolute"></div>
-      <div className="container">
+      <div
+        className="absolute h-64 w-64 bg-purple-500 rounded-full border top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 border-white/20
+      bg-[radial-gradient(50%_50%_at_16.8%_18.3%,white,rgb(182,148,255)_37.7%,rgb(24,0,66))] shadow-[-20px_-20px_50px_rgb(255,255,255,.5),-20px_-20px_80px_rgb(255,255,255,0.1)]"
+      ></div>
+      <div className="container relative">
         <h1
           className="text-8xl font-semibold tracking-tighter bg-white text-transparent bg-clip-text text-center 
         bg-[radial-gradient(100%_100%_at_top_left,white,white,rgb(74,32,138,.5))]"
