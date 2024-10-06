45af8d1c8827954cb6f017f85d5a24592659419d
Yafet Zerihun <zerihunyafet80@gmail.com>
2024-10-06 18:08:24 +0300

    update the header section button with the resusable button and udpate the route

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,6 +1,6 @@
 import LogoIcon from "@/assets/logo.svg";
 import MenuIcon from "@/assets/icon-menu.svg";
-
+import { Button } from "@/components/Button";
 export const Header = () => {
   return (
     <header className="py-4 border-b border-white/15 md:border-none">
@@ -40,17 +40,7 @@ export const Header = () => {
             </div>
 
             <div className="flex gap-4 items-center">
-              <button
-                className="relative py-2 px-3 rounded-lg font-medium
-              text-sm bg-gradient-to-b from-[#190d2e] to-[#4a208a] shadow-[0px_0px_12px_#8c45ff]"
-              >
-                <div className="absolute inset-0">
-                  <div className="border border-white/20 aboslute inset-0 [mask-image:linear-gradient(to_bottom,black,transparent)]"></div>
-                  <div className="border absolute inset-0 border-white/40 [mask-image:linear-gradient(to_top,black,transparent)]"></div>
-                  <div className="absolute shadow-[0_0_10px_rgb(140,69,255,.7)_inset] rounded-lg"></div>
-                </div>
-                <span>Join waitlist</span>
-              </button>
+              <Button>Join waitlist</Button>
               <span>
                 <MenuIcon className="md:hidden" />
               </span>
