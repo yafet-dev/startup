92cb43e2df12d6fa75b781fe05b9f2d4aebad244
Yafet Zerihun <zerihunyafet80@gmail.com>
2024-10-07 00:30:05 +0300

    make the trusted by section responsive

--- src/sections/LogoTicker.tsx
+++ src/sections/LogoTicker.tsx
@@ -7,10 +7,10 @@ import echoLogo from "@/assets/logo-echo.png";
 
 export const LogoTicker = () => {
   return (
-    <section className="py-20">
+    <section className="py-20 md:py-24">
       <div className="container">
-        <div className="flex items-center">
-          <div className="flex-1">
+        <div className="flex items-center gap-5">
+          <div className="flex-1 md:flex-none">
             <h2>Trusted by top innovate teams</h2>
           </div>
           <div className="flex-1 overflow-hidden [mask-image:linear-gradient(to_right,transparent,black_20%,black_80%,transparent)]">
