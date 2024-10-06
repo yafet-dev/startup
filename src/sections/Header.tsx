884d73324cf7954393ebf680fc6adbe6bbae94b7
Yafet Zerihun <zerihunyafet80@gmail.com>
2024-10-06 16:12:21 +0300

    fix header style

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -3,12 +3,31 @@ import MenuIcon from "@/assets/icon-menu.svg";
 
 export const Header = () => {
   return (
-    <header className="py-4 border-b border-white/15">
+    <header className="py-4 border-b border-white/15 md:border-none">
       <div className="container">
         <div>
-          <div className="flex justify-between">
-            <div className="border h-10 w-10 rounded-lg inline-flex justify-center items-center border-white/15">
-              <LogoIcon className="border h-8 w-8 " />
+          <div className="flex justify-between items-center border">
+            <div>
+              <div className="border h-10 w-10 rounded-lg inline-flex justify-center items-center border-white/15">
+                <LogoIcon className="border h-8 w-8 " />
+              </div>
+            </div>
+
+            <div>
+              <nav className="flex gap-8  transition">
+                <a href="#" className=" text-white/70 hover:text-white">
+                  Features
+                </a>
+                <a href="#" className=" text-white/70 hover:text-white">
+                  Developers
+                </a>
+                <a href="#" className=" text-white/70 hover:text-white">
+                  Pricing
+                </a>
+                <a href="#" className=" text-white/70 hover:text-white">
+                  Changelog
+                </a>
+              </nav>
             </div>
 
             <div className="flex gap-4 items-center">
@@ -19,6 +38,7 @@ export const Header = () => {
                 <div className="absolute inset-0">
                   <div className="border border-white/20 aboslute inset-0 [mask-image:linear-gradient(to_bottom,black,transparent)]"></div>
                   <div className="border absolute inset-0 border-white/40 [mask-image:linear-gradient(to_top,black,transparent)]"></div>
+                  <div className="absolute shadow-[0_0_10px_rgb(140,69,255,.7)_inset] rounded-lg"></div>
                 </div>
                 <span>Join waitlist</span>
               </button>
