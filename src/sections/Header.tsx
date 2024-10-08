13e9d4c68a424dead8dc5a2256534ce13660590b
Yafet Zerihun <zerihunyafet80@gmail.com>
2024-10-08 12:44:47 +0300

    fix the header backdrop for mobile

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -4,9 +4,10 @@ import { Button } from "@/components/Button";
 export const Header = () => {
   return (
     <header className="py-4 border-b border-white/15 md:border-none mb-4 md:-mb-12 sticky top-0 z-10 ">
+      <div className="absolute inset-0 backdrop-blur-sm -z-10 md:hidden"></div>
       <div className="container">
         <div>
-          <div className="flex justify-between items-center md:border border-white/15 md:p-2.5 rounded-xl max-w-2xl mx-auto backdrop-blur-2xl">
+          <div className="flex justify-between items-center md:border border-white/15 md:p-2.5 rounded-xl max-w-2xl mx-auto md:backdrop-blur-md">
             <div>
               <div className="border h-10 w-10 rounded-lg inline-flex justify-center items-center border-white/15">
                 <LogoIcon className="border h-8 w-8 " />
