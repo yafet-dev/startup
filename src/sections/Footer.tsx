ac73b35b196f509e411cff124558d40b5439600b
Yafet Zerihun <zerihunyafet80@gmail.com>
2024-10-18 15:58:10 +0300

    footer setup done

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,3 +1,30 @@
+import Logo from "@/assets/logo.svg";
+import Xsocial from "@/assets/social-x.svg";
+import InstaSocial from "@/assets/social-instagram.svg";
+import YTSocial from "@/assets/social-youtube.svg";
 export const Footer = () => {
-  return <footer>Footer</footer>;
+  return (
+    <footer>
+      <div>
+        <div>
+          <Logo />
+          <div>Web Weave Landing Page</div>
+        </div>
+        <div>
+          <nav>
+            <a href="#">Features</a>
+            <a href="#">Developers</a>
+            <a href="#">Company</a>
+            <a href="#">Blog</a>
+            <a href="#">Changelog</a>
+          </nav>
+        </div>
+        <div>
+          <Xsocial />
+          <InstaSocial />
+          <YTSocial />
+        </div>
+      </div>
+    </footer>
+  );
 };
