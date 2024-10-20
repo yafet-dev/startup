70a76f13ee28ab8db8468d3d402d0c64c5ad72a1
Yafet Zerihun <zerihunyafet80@gmail.com>
2024-10-21 01:31:29 +0300

    add cta animation

--- src/sections/CallToAction.tsx
+++ src/sections/CallToAction.tsx
@@ -1,36 +1,105 @@
-import { Button } from "@/components/Button";
-import starsBg from "@/assets/stars.png";
-import gridLines from "@/assets/grid-lines.png";
+"use client";
 
-export const CallToAction = () => {
-  return (
-    <section className="py-20 md:py-24">
-      <div className="container">
-        <div
-          className="border border-white/15 py-24 rounded-xl overflow-hidden relative"
-          style={{
-            backgroundImage: `url(${starsBg.src})`,
-          }}
-        >
-          <div
-            className="absolute inset-0 bg-[rgb(74,32,138)] bg-blend-overlay [mask-image:radial-gradient(50%_50%_at_50%_35%,black,transparent)]"
-            style={{
-              backgroundImage: `url(${gridLines.src})`,
-            }}
-          ></div>
-          <div className="relative">
-            <h2 className="text-5xl md:text-6xl max-w-sm mx-auto tracking-tighter text-center font-medium">
-              AI-driven SEO for everyone
-            </h2>
-            <p className="text-center md:text-xl max-w-sm mx-auto text-lg text-white/70 px-4 mt-5 tracking-tight">
-              Achieve clear, impactful results without the complexity.
-            </p>
-            <div className="flex justify-center mt-8">
-              <Button>Join waitlist</Button>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
-  );
+import { Button } from "@/components/button";
+import BackgroundStars from "@/assets/stars.png";
+import BackgroundGrid from "@/assets/grid-lines.png";
+import {
+  motion,
+  useMotionTemplate,
+  useMotionValue,
+  useScroll,
+  useTransform,
+} from "framer-motion";
+import { RefObject, useEffect, useRef } from "react";
+
+{
+  /* Custom Hook for Relative Mouse Position */
+}
+const useRelativeMousePosition = (to: RefObject<HTMLElement>) => {
+  const mouseX = useMotionValue(0);
+  const mouseY = useMotionValue(0);
+
+  const updateMousePosition = (event: MouseEvent) => {
+    if (!to.current) return;
+    const { top, left } = to.current.getBoundingClientRect();
+    mouseX.set(event.x - left);
+    mouseY.set(event.y - top);
+  };
+
+  useEffect(() => {
+    window.addEventListener("mousemove", updateMousePosition);
+    return () => window.removeEventListener("mousemove", updateMousePosition);
+  });
+  return [mouseX, mouseY];
 };
+
+export function CallToAction() {
+  const sectionRef = useRef<HTMLElement>(null);
+  const borderedDivRef = useRef<HTMLDivElement>(null);
+  const { scrollYProgress } = useScroll({
+    target: sectionRef,
+    offset: [`start end`, "end start"],
+  });
+  const backgroundPositionY = useTransform(
+    scrollYProgress,
+    [0, 1],
+    [-300, 300]
+  );
+
+  const [mouseX, mouseY] = useRelativeMousePosition(borderedDivRef);
+  const maskImage = useMotionTemplate`radial-gradient(50% 50% at ${mouseX}px ${mouseY}px, black, transparent)`;
+
+  return (
+    <>
+      <section className={"py-20 md:py-24"} ref={sectionRef}>
+        <div className={"container"}>
+          <motion.div
+            animate={{ backgroundPositionX: BackgroundStars.width }}
+            transition={{ duration: 120, repeat: Infinity, ease: "linear" }}
+            className={
+              "border border-muted py-24 px-6 rounded-xl overflow-hidden relative group"
+            }
+            style={{
+              backgroundImage: `url(${BackgroundStars.src})`,
+              backgroundPositionY,
+            }}
+          >
+            <div
+              className={
+                "absolute inset-0 bg-[rgb(74,32,138)] bg-blend-overlay [mask-image:radial-gradient(50%_50%_at_50%_35%,black,transparent)] group-hover:opacity-0 transition duration-700"
+              }
+              style={{ backgroundImage: `url(${BackgroundGrid.src})` }}
+            />
+            <motion.div
+              className={
+                "absolute inset-0 bg-[rgb(74,32,138)] bg-blend-overlay opacity-0 group-hover:opacity-100 transition duration-700"
+              }
+              style={{
+                backgroundImage: `url(${BackgroundGrid.src})`,
+                maskImage: maskImage,
+              }}
+              ref={borderedDivRef}
+            />
+            <div className={"relative"}>
+              <h2
+                className={"text-5xl tracking-tighter text-center font-medium"}
+              >
+                AI-driven SEO for everyone
+              </h2>
+              <p
+                className={
+                  "text-center text-lg md:text-xl text-white/70 tracking-tight px-4 mt-5"
+                }
+              >
+                Achieve clear, impactful results without the complexity.
+              </p>
+              <div className={"flex justify-center mt-8"}>
+                <Button>Join waitlist</Button>
+              </div>
+            </div>
+          </motion.div>
+        </div>
+      </section>
+    </>
+  );
+}
