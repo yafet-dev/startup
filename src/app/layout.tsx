8e3b47976fa0e93cd98ad9585e348e1cfa6679f6
Yafet Zerihun <zerihunyafet80@gmail.com>
2024-11-04 11:35:06 +0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,8 +6,8 @@ import { twMerge } from "tailwind-merge";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "AI Startup Landing Page",
-  description: "A landing page for an AI startup created with Frontend Tribe",
+  title: "WebWeave,
+  description: "A landing page for an AI startup",
 };
 
 export default function RootLayout({
